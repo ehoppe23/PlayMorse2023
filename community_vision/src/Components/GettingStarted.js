b1ebdd3213249db06abb76a5d91f51db02a52306
tashchuk23 <94871535+natalietash@users.noreply.github.com>
2023-04-10 21:48:39 -0700

    Update GettingStarted.js
    improve readability of instructions page (GettingStarted.js) by simplifying and adding line breaks between headings. Added description of Burger Stack and Alphabet Clearout

--- community_vision/src/Components/GettingStarted.js
+++ community_vision/src/Components/GettingStarted.js
@@ -28,23 +28,30 @@ const GettingStarted = forwardRef((props, ref) => {
       color: fontColor,
 
     }}>
-      <h1 style={{ fontWeight: 900, fontSize: "50px", paddingTop: '20px' }}>Typing with Morse on this Website</h1>
+      <h1 style={{ fontWeight: 900, fontSize: "50px", paddingTop: '20px' }}>Instructions and Game Descriptions</h1>
 
-      <h2 style={{ fontWeight: 900, fontSize: "30px", textAlign: "left", paddingLeft: "10vw" }}>How to type with Morse on this Website:</h2>
+      <h2 style={{ fontWeight: 900, fontSize: "40px", textAlign: "left", paddingLeft: "10vw" }}>How to type with Morse on this Website:</h2>
 
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }} >Morse code is made up of a combination of 'dots' and 'dashes' to create letters
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }} >Morse code uses a combination of 'dots' and 'dashes' to create letters
             and numbers. Click here for a <a href="https://cvision.org/wp-content/uploads/2021/02/Morse-Code-Guide.pdf" target="_blank">PDF of a Morse code guide.</a></p>
 
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>On this website, each activity requires you to type a 'dot or 'dash' using a keyboard,
-        switches, or a mouse.</p>
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>There are 3 possible ways to input the 'dot' and 'dash' on this website: the mouse, the space and enter keys, or a switch device.</p>
 
+      <br></br>
       <h2 style={{ fontWeight: 900, fontSize: "30px", textAlign: "left", paddingLeft: "10vw" }}>Using a Keyboard:</h2>
+
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>Dot and dash are activated by <b>keyboard keys:</b></p>
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>Space = 'dot'</p>
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>Enter = 'dash'</p>
 
+      <br></br>
+      <h2 style={{ fontweight: 900, fontSize: "30px", textAlign: "Left", paddingLeft: "10vw" }}>Using a Mouse:</h2>
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>Simply click on the 'dot' and 'dash' on the screen in each activity.</p>
+
+
+      <br></br>
       <h2 style={{ fontWeight: 900, fontSize: "30px", textAlign: "left", paddingLeft: "10vw" }}>Using Switches:</h2>
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>Like using a keyboard, you will use the 'space' and 'enter' keys to
@@ -57,45 +64,42 @@ const GettingStarted = forwardRef((props, ref) => {
           your switch interface keyboard commands to 'space' and 'enter'.
         </p>
 
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>If you are using left and right sides of the body for swich placement,
-        place the switches like this:</p>
 
 
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "Left", fontSize: "25px" }}>If you are using left and right sides of the body for switch placement, place
-      the switches like this:
-        </p>
-
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>'space' (dot) - Left side</p>
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>'enter' (dash) - Right side</p>
-
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>
-        This placement will visually match how the 'dot' and 'dash' are placed on the
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "Left", fontSize: "25px" }}>If you are using the left and right sides of the body for switch placement, place
+      the 'space' switch (dot) on the left, and the 'enter' switch (dash) on the right.  This placement will visually match how the 'dot' and 'dash' are displayed on the
         screen.
         </p>
 
+
+    
+
       <img src={buttons} alt="Photo of dot and dash buttons" id="buttonsimage" width="60%"></img>
 
-      <h2 style={{ fontweight: 900, fontSize: "40px", textAlign: "Left", paddingLeft: "10vw" }}>Using a Mouse:</h2>
+      
 
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>Simply click on the 'dot' and 'dash' on the screen in each activity.</p>
-
-      <h2 style={{ fontweight: 900, fontSize: "40px", textAlign: "Left", paddingLeft: "10vw" }}>Settings:</h2>
+      <br></br>
+      <h2 style={{ fontweight: 900, fontSize: "40px", textAlign: "Left", paddingLeft: "10vw" }}>Customizing your Settings:</h2>
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>Each person may do better with different color combinations, font sizes,
-        volume, or speeds so our settings let you customize: </p>
+        volume, or speeds so our settings page let you customize: </p>
 
       <ol style={{ paddingLeft: "10vw", paddingRight: "15vw", textAlign: "left", fontSize: "25px" }}>
-        <li>Color of background, buttons, and text</li>
-        <li>Size of text</li>
-        <li>Sound level (volume)</li>
-        <li>How quickly switches or keys need to be hit to make a letter</li>
+        <li>The color of the background, buttons, and text</li>
+        <li>The size of text</li>
+        <li>The sound level (volume)</li>
+        <li>How quickly the switches or keys need to be hit to make a letter (Game Speed)</li>
       </ol>
 
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>The settings page may be accessed at any time by clicking the gear icon in the top right corner labeled 'settings'. </p>
+
+      <br></br>
       <h2 style={{ fontweight: 900, fontSize: "40px", textAlign: "left", paddingLeft: "10vw" }}>Description of the Games</h2>
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>In most activities, you can choose to include Morse code prompts (visual cues for correct Morse combination) 
         or turn them off to make the activity more challenging after you have practiced for a while. In some activities you can choose to ‘scramble’ the order of the letters.</p>
 
+      <br></br>
       <h2 style={{ fontweight: 600, color: "blue", textAlign: "Left", paddingLeft: "10vw", fontSize: "30px" }}>Learn Morse Basics:</h2>
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>‘Explore Dot & Dash’ gives a player opportunities to discover how to make a ‘dot’ and ‘dash’ using a keyboard or switches. This is a good place to start to learn how switches (or keystrokes) work on this website.</p>
@@ -106,26 +110,32 @@ const GettingStarted = forwardRef((props, ref) => {
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>‘3 Hits’ and ‘4 Hits’ ask for letters that use three or four hits, accordingly.</p>
 
+
+    
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>‘Learn Morse Patterns’ includes the entire alphabet but presents the letters in order of Morse pattern complexity. It starts with the simplest Morse patterns (example, one ‘dot’ to make the letter E) and progresses through more complex patterns (up to 4 switch hits).</p>
 
-
+      <br></br>
       <h2 style={{ fontweight: 900, color: "blue", fontSize: "30px", textAlign: "left", paddingLeft: "10vw" }}>Learn Letters:</h2>
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", textAlign: "left", fontSize: "25px" }}>'Learn the Alphabet' and 'Learn Numbers' each walks a player
         through using Morse code to type the whole alphabet or numbers 0-9.</p>
 
 
+      <br></br>
       <h2 style={{ fontweight: 900, color: "blue", fontSize: "30px", textAlign: "left", paddingLeft: "10vw" }}>Learn Words: </h2>
 
       <p style={{ paddingLeft: "10vw", paddingRight: "10vw", fontSize: "25px", textAlign: "left" }}>‘Learn Words’ introduces a player to Morse code to type whole words. </p>
 
-
+      <br></br>
       <h2 style={{ fontweight: 900, color: "blue", fontSize: "30px", textAlign: "left", paddingLeft: "10vw" }}>Themed Games:</h2>
 
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", fontSize: "25px", textAlign: "left" }}>Car Race Game presents randomized letters that need to be entered with the correct Morse pattern before the letter crossed the screen. This is for an advanced player since there are no prompts and it has a timed element!</p>
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", fontSize: "25px", textAlign: "left" }}>'Car Race Game' presents randomized letters that need to be entered with the correct Morse pattern before the letter crossed the screen. This is for an advanced player since there are no prompts and it has a timed element!</p>
 
-      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", fontSize: "25px", textAlign: "left" }}>‘Sandbox’ games let you explore making any ‘dot’ and ‘dash’ combinations to see what letter or word you type. There are no target letters or words to produce, only experimentation!</p>
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", fontSize: "25px", textAlign: "left" }}>‘Sandbox games' lets you explore making any ‘dot’ and ‘dash’ combinations to see what letter or word you type. There are no target letters or words to produce, only experimentation!</p>
+     
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", fontSize: "25px", textAlign: "left" }}>‘Burger Stack' also lets you explore 'dot' and 'dash combinations to make any letter to add a piece to the hamburger.</p>
 
+      <p style={{ paddingLeft: "10vw", paddingRight: "10vw", fontSize: "25px", textAlign: "left" }}>‘Alphabet Clearout' challenges the user to type every Morse letter and clear the entire alphabet!</p>
 
       <Link className='nav-link' to="/" style={{
         backgroundColor: backgroundColor
